47d7f9a969612a901d01d95322d0287107589bc5
junanhuang583-droid <junanhuang583@gmail.com>
2026-09-13 03:52:29 +0800

    Point app to Voxel Island v1

--- _reset/src/main.js
+++ _reset/src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';
-import { createIsland3D } from './scene.js';
+import { createVoxelIslandV1 } from './scene.js';
 
-createIsland3D(THREE, OrbitControls, document.querySelector('#app'));
+createVoxelIslandV1(THREE, OrbitControls, document.querySelector('#app'));
