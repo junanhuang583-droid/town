0314958ade54fe4cdbf968f7af4862659f07b9a0
junanhuang583-droid <junanhuang583@gmail.com>
2026-09-13 04:27:48 +0800

    Switch preview to Voxel Island v2

--- _reset/src/preview.js
+++ _reset/src/preview.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://esm.sh/three@0.180.0';
 import { OrbitControls } from 'https://esm.sh/three@0.180.0/examples/jsm/controls/OrbitControls.js';
-import { createVoxelIslandV1 } from './scene.js';
+import { createVoxelIslandV2 } from './scene.js';
 
-createVoxelIslandV1(THREE, OrbitControls, document.querySelector('#app'));
+createVoxelIslandV2(THREE, OrbitControls, document.querySelector('#app'));
