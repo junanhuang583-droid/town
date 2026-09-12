5c5657c6911245c91024d706a4abc964503a6c08
junanhuang583-droid <junanhuang583@gmail.com>
2026-09-13 04:27:52 +0800

    Switch app to Voxel Island v2

--- _reset/src/main.js
+++ _reset/src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';
-import { createVoxelIslandV1 } from './scene.js';
+import { createVoxelIslandV2 } from './scene.js';
 
-createVoxelIslandV1(THREE, OrbitControls, document.querySelector('#app'));
+createVoxelIslandV2(THREE, OrbitControls, document.querySelector('#app'));
